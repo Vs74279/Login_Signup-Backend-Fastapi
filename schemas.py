639e20b1c499a96d30831db4a5aa5199684e04b1
vikash kumar singh <drv74279@gmail.com>
2024-06-25 15:31:57 +0530

    first commit

---
+++ schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+ 
+class UserBase(BaseModel):
+    email: str
+ 
+class UserCreate(UserBase):
+    firstName: str
+    lastName: str
+    phone: str
+    password: str
+    qualification: str
+ 
+class UserLogin(BaseModel):
+    email: str
+    password: str   
